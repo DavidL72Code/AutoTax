12e1818a7776ee46dbbbb3c1afb66d90d9d40891
DavidL72Code <davidducle2@gmail.com>
2026-01-24 00:30:11 -0500

    added ai and changed regex parsing to use same parsing pattern/style as generic parser

--- python-service/app/parsers/amazon_parser.py
+++ python-service/app/parsers/amazon_parser.py
@@ -1,23 +1,43 @@
 import re 
 from datetime import datetime
 from dateutil import parser as date_parser
+from generic_parser import regex_parsing
+from generic_parser import ai_search
 
-def amazon_parser(email_text:str,email_id:str)-> dict:
+def amazon_parser(email_text:str,email_id:str,vendor_name:str)-> dict:
     information={
         'email_id': email_id,
         'vendor': "amazon",
         'email body': email_text
     }
-    amount_match= re.search(r'(?:Total|Order Total).*?\$?([\d,]+\.\d{2})', email_text, re.IGNORECASE)
-    if amount_match:
-        information["amount"]=float(amount_match.group(1).replace(',',''))
-    date_match=re.search(r'date[:\s]+([^\n]+)',email_text,re.IGNORECASE)
-    if date_match:
-        try:
-            information["date"]=date_parser.parse(date_match.group(1),fuzzy=True)
-        except:
-            pass 
-    tax_match=re.search(r'tax[:\s]+\$?([\d,]+\.\d{2})',email_text,re.IGNORECASE)
-    if tax_match:
-        information["tax"]=float(tax_match.group(1).replace(',',''))
-    return information
+    regex_result=regex_parsing(email_text)
+    result.update(regex_result)
+
+    has_amount=regex_result.get('amount') is not None
+    has_date=regex_result.get('date') is not None
+    has_tax=regex_result.get('tax') is not None
+
+    if not has_amount or not has_date or has_tax:
+        missing_info=[]
+        if not has_amount:
+            missing_info.append('amount')
+        if not has_date:
+            missing_info.append('date')
+        if not has_tax:
+            missing_info.append('tax')
+        print(f" Regex failed for {vendor_name}:{','.join(missing_info)} mssing")
+        print("\n Using Ai to extract missing fields\n")
+
+        ai_result=ai_search(email_text,missing_info)
+        if ai_result.get('amount') and not has_amount:
+            result['amount'] = ai_result['amount']
+        
+        if ai_result.get('date') and not has_date:
+            result['date'] = ai_result['date']
+        
+        if ai_result.get('tax') and not result.get('tax'):
+            result['tax'] = ai_result['tax']
+    else:
+        print(f"Regex extraction successful for {vendor_name}")
+    
+    return result
